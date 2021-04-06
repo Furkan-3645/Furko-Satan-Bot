cbccf99d05d82c57d241af653307b87206382f5f
Furkan-3645 <80989865+Furkan-3645@users.noreply.github.com>
2021-04-06 21:05:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,19 +2,25 @@ const kimlik = 'ODI2ODIyODMxOTcwMTg5MzEy.YGSExA.eDi-v2QCnp8aq1poFXM8bSj9IVQ';
 const Sahip_ID = '783333981483171840'
 const prefix = ''
 const Discord = require('discord.js');
-const Bot = new Discord.Client();
+const Client = new Discord.Client();
 
-Bot.on('ready', () => {
+Client.on('ready', () => {
   console.log(`Bir Bot Çalışıyor = ${Bot.user.tag}`);
   Bot.user.setActivity('Furko Gazoz Satma Oyunu', { type: 'PLAYING' })
   .then(presence => console.log(`${Bot.user.tag} Botunun Durumu ${presence.activities[0].name}`))
   .catch(console.error);
 });
 
-Bot.on('message', mesaj => {
+Client.on('message', mesaj => {
   if (mesaj.content === prefix + 'sa') {
     mesaj.channel.send('ooo hoşgeldi  ngfn');
   }
+});
+
+Client.on('message', Mesaj => {
+    if (Mesaj.content.toLowerCase() === 'Mesaj') {
+      Mesaj.react('Verilecek tepki')
+    }
 });
 
 Bot.login(kimlik);
