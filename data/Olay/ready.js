e5f913db54eedbc907a98fce73f26272d046b7f8
Furkan-3645 <80989865+Furkan-3645@users.noreply.github.com>
2021-06-05 03:19:35 +0300

    Delete data directory

--- data/Olay/ready.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const moment = require('moment');
-const chalk = require('chalk');
-const { prefix } = require('../ayarlar.json')
-
-module.exports = client => {
-  client.user.setStatus('dnd'); //dnd, idle, online, offline
-  
-}
